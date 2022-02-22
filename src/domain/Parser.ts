73cdcd0a15c0e2d302189ca8e9e1ce00a7a9e70d
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-02-22 10:07:58 -0300

    get number literal from tokenizer

--- src/domain/Parser.ts
+++ src/domain/Parser.ts
@@ -1,27 +1,50 @@
 import { Tokenizer } from "./Tokenizer";
 import { NumericLiteralI } from "../interfaces/NumericLiteralI";
+import { TokenI } from "../interfaces/TokenI";
 export class Parser {
   private expression: string;
+  private tokenizer: Tokenizer;
+  private currentToken: TokenI | null;
 
   constructor() {
     this.expression = '';
+    this.tokenizer = new Tokenizer();
+    this.currentToken = null;
   }
 
   parse(expression: string) {
     this.expression = expression;
+    this.tokenizer.init(expression);
+    this.currentToken = this.tokenizer.getNextToken();
+    return this;
   }
 
   Program() {
-    return this.NumericLiteral();
+    return {
+      type: 'Program',
+      body: this.NumericLiteral()
+    }
   }
-
   NumericLiteral(): NumericLiteralI {
+    const token = this.getToken("NUMBER")
     return {
       type: "NumericLiteral",
-      value: Number(this.expression)
+      value: Number(token.value)
     }
   }
 
+  getToken(tokenType: string) {
+    const token = this.currentToken;
+    if (token === null) {
+      throw new SyntaxError(`Unespected end of input, expected: ${tokenType}`);
+    }
 
+    if (token.type !== tokenType) {
+      throw new SyntaxError(`Unespected token: "${token.value}", expected: ${tokenType}`);
+    }
+
+    this.currentToken = this.tokenizer.getNextToken();
+    return token;
+  }
 }
 
--- tests/run.ts
+++ tests/run.ts
@@ -2,8 +2,7 @@ import { Parser } from '../src/domain/Parser';
 
 const parser: Parser = new Parser();
 const program: string = `79`;
-parser.parse(program);
-const ast = parser.Program();
+const ast = parser.parse(program).Program();
 
 console.log(JSON.stringify(ast, null, 2));
 
